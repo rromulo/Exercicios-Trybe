59ea98bea09b736a906b019fba5e51a35ecfdee9
rromulo <rromulo.silva21@gmail.com>
2022-07-21 20:11:13 -0300

    Adiciona HOF que retorna objeto no formato {fullName, email}

---
+++ fundamentos/Bloco8/dia1-hofs/script.js
@@ -0,0 +1,14 @@
+const newCollaborator = (person) => {
+  let colaborador = `${person}, ${person.split(' ').join('_').toLowerCase()}@trybe.com`;
+  return colaborador;
+}
+
+const newEmployees = (func) => {
+  const employees = {
+    id1: func('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: func('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: func('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
+  return employees;
+};
+console.log(newEmployees(newCollaborator));
