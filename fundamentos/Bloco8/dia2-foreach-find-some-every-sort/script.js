e597a6ab2d65a5558b3ae02856097b50a4274f0d
rromulo <rromulo.silva21@gmail.com>
2022-07-23 00:43:25 -0300

    Adiciona função que retorna o livro com tamanho de 26 caracteres e função que ordena livros em ordem decrescente

--- fundamentos/Bloco8/dia2-foreach-find-some-every-sort/script.js
+++ fundamentos/Bloco8/dia2-foreach-find-some-every-sort/script.js
@@ -60,3 +60,100 @@ const books = [
     releaseYear: 1928,
   },
 ];
+// Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947
+// nome da pessoa
+// primeira pessoa autora
+// nascida em 1947
+function authorBornIn1947() {
+  return books.find((pessoa) => {
+    if (pessoa.author.birthYear === 1947) {
+      console.log (pessoa.author.name)
+    }
+  })
+}
+authorBornIn1947();
+
+//livro de menor nome
+function smallerName(livros) {
+  let nameBook;
+  let arraybook = [];
+  livros.forEach((book) => {
+    arraybook.push(book.name);
+  })
+  arraybook.sort((a,b) => a.length - b.length)
+  nameBook = arraybook[0]
+  // Variável nameBook que receberá o valor do menor nome;
+  return nameBook;
+}
+console.log(smallerName(books));
+
+// Encontre o primeiro livro cujo nome possui 26 caracteres.
+const expectedResult = {
+  author: {
+    birthYear: 1948,
+    name: 'George R. R. Martin',
+  },
+  genre: 'Fantasia',
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  releaseYear: 1991,
+};
+
+function getNamedBook(book) {
+  // escreva seu código aqui
+  arrayBook = Object.values(book);
+  return arrayBook.find((nameBook) => nameBook.length === 26);
+}
+console.log(getNamedBook(expectedResult));
+
+const expectedResultTwo = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: { name: 'George R. R. Martin', birthYear: 1948 },
+    releaseYear: 1991,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: { name: 'Stephen King', birthYear: 1947 },
+    releaseYear: 1986,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: { name: 'Frank Herbert', birthYear: 1920 },
+    releaseYear: 1965,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+    releaseYear: 1928,
+  },
+];
+
+// Ordene os livros por data de lançamento em ordem decrescente.
+function booksOrderedByReleaseYearDesc(books) {
+  // escreva aqui seu código
+  return books.sort((a, b) => a.releaseYear - b.releaseYear ? 1 : -1)
+}
+console.log(booksOrderedByReleaseYearDesc(expectedResultTwo));
